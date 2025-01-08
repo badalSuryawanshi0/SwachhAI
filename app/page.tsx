95fc15defa0400d4e3d320ca3b52082f0c464993
Badal <Badalsuryawanshi0@gmail.com>
2025-01-09 01:39:57 +0530

    Fix build

--- app/page.tsx
+++ app/page.tsx
@@ -30,8 +30,8 @@ export default function Home() {
           Welcome <span className="text-green-600">Swaach Bharat</span>
         </h1>
         <p className="text-xl text-gray-600 max-w-2xl mx-auto leading-relaxed mb-8">
-          "Transforming India, One Step at a Time. Join Swachh Bharat App in
-          revolutionizing waste management for a cleaner, greener future!"
+          &quot;Transforming India, One Step at a Time. Join Swachh Bharat App in
+          revolutionizing waste management for a cleaner, greener future!&quot;
         </p>
         {!loggedIn ? (
           <Button className="bg-green-600 hover:bg-green-700 text-white text-lg py-6 px-10 rounded-full font-medium transition-all duration-300 ease-in-out transform hover:scale-105">
